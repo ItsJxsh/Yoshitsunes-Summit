eb1e57a8e1600932ae886d9232294289524f9fa7
ccrlc06 <200569393@student.georgianc.on.ca>
2023-12-08 22:39:40 -0500

    Added options menu and sound bar

--- Assets/scripts/Buttons/buttons.cs
+++ Assets/scripts/Buttons/buttons.cs
@@ -2,9 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class button_control : MonoBehaviour
 {
+    public Slider soundSlider;
+    public GameObject audioManager; 
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = audioManager.GetComponent<AudioSource>();
+
+        soundSlider.value = audioSource.volume;
+
+        soundSlider.onValueChanged.AddListener(OnSoundSliderChanged);
+    }
+
+  
+
     public void Retry()
     {
         SceneManager.LoadScene("TutorialStage");
@@ -14,4 +30,10 @@ public class button_control : MonoBehaviour
     {
         Application.Quit();
     }
+
+    void OnSoundSliderChanged(float volume)
+    {
+        audioSource.volume = volume;
+    }
+
 }
