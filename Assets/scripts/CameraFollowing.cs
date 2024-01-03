9ded295b479466caad9212402264ca0a02bda2da
MatthewAlbisser <Matthewalbisser@gmail.com>
2024-01-03 01:51:19 -0500

    Camera Boundary
    -Added square barrier points to scene in script.
    -Camera will stay inside set boundary.

--- Assets/scripts/CameraFollowing.cs
+++ Assets/scripts/CameraFollowing.cs
@@ -3,20 +3,25 @@ using static UnityEngine.GraphicsBuffer;
 
 public class CameraFollowing : MonoBehaviour
 {
-    public Transform player;                // Reference to player position.
-    public Vector3 camPosition;             // Position of the camera relative to the player.
+
+    // Camera movement flow w/ player.
+    public Transform player;                // Reference player position.
+    private Vector3 camPosition;            // Position of camera relative to the player.
     public float smoothSpeed = 0.125f;      // Smoothing factor for camera movement.
-
-    private float lastMoveDirection = 0f;   // Float for horizontal offset hold.
-
     public float horizontalOffset = 0.75f;  // Offset to adjust the camera horizontally.
     public float verticalOffset = 2f;       // Offset to adjust the camera vertically.
 
-    void LateUpdate()
+    private float lastMoveDirection;
+
+    // Square camera movement barrier points.
+    public Vector2 minBoundary = new Vector2(5f, 4f);   // Camera barrier XY coords, bottom left of scene
+    public Vector2 maxBoundary = new Vector2(103f, 26f);   // Camera barrier XY coords, top right of scene
+
+    void Update()
     {
-        if (player != null)     
+        if (player != null)
         {
-            float horizontalInput = Input.GetAxisRaw("Horizontal"); 
+            float horizontalInput = Input.GetAxisRaw("Horizontal");
             float verticalInput = Input.GetAxisRaw("Vertical");
 
             if (horizontalInput != 0)
@@ -32,8 +37,10 @@ public class CameraFollowing : MonoBehaviour
             Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
             transform.position = smoothedPosition;                      // Smoothly moves the camera towards the desired position.
 
-            //(To freeze Z axis, not a good fix)
-            transform.position = new Vector3(transform.position.x, transform.position.y, -10f);
+            transform.position = new Vector3(transform.position.x, transform.position.y, -10f); // To freeze Z axis cam movement, needs better fix.
+
+            // Keeps Camera within boundary points. 
+            transform.position = new Vector3(Mathf.Clamp(smoothedPosition.x, minBoundary.x, maxBoundary.x),Mathf.Clamp(smoothedPosition.y, minBoundary.y, maxBoundary.y),transform.position.z);
         }
     }
 }
